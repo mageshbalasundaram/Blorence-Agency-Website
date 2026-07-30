cd2a527c745681ec2a71ea775b89711b03cc9f7f
mageshbalasundaram <mageshbabu2717@gmail.com>
2026-07-31 02:38:09 +0530

    final deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      filename: "bundle-report.html",
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
 });
