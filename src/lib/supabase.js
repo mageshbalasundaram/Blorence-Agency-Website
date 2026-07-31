23cb34791f133d055f224ed633da1b530f3762ec
mageshbalasundaram <mageshbabu2717@gmail.com>
2026-07-31 22:14:15 +0530

    Debug Supabase env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,6 +4,7 @@ console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
 console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export const supabase = createClient(
+    
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 );
